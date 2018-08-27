d868e7eea6dec7c229b101336072aee32f530714
zoucong <1481435952@qq.com>
2018-08-27 14:13:08 +0800

    v1.0

---
+++ src/test/java/com/zc/hdfs/HdfsTest.java
@@ -0,0 +1,15 @@
+package com.zc.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Test;
+
+public class HdfsTest {
+
+    @Test
+    public void test1(){
+        Configuration conf = new Configuration();
+
+
+    }
+
+}
